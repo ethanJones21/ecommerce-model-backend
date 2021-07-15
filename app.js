f8ecb22d8070e69d740a8673f92f824f1e53bc98
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-15 01:25:01 -0500

    add corse in app.js

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ const mongoose = require("mongoose");
 const port = process.env.port || 4201;
 
 mongoose.connect("mongodb://127.0.0.1:27017/tienda", (err, res) =>
-  err
-    ? console.log(err)
-    : app.listen(port, () => console.log("Server run in port " + port))
+    err
+        ? console.log(err)
+        : app.listen(port, () => console.log("Server run in port " + port))
 );
+
+// CORSE
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, PUT, POST, DELETE, OPTIONS"
+    );
+    res.header("Allow", "GET, PUT, POST, DELETE, OPTIONS");
+    next();
+});
 
 module.exports = app;
