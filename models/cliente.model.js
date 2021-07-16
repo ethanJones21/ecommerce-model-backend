794ea10dfd538abedc115b1372e8bcc620963b58
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-16 12:42:49 -0500

    change nombres:nombre and apellidos:apellido

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ClienteSchema = Schema({
-    nombres: { type: String, required: true },
-    apellidos: { type: String, required: true },
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
     pais: { type: String, required: false },
     email: { type: String, required: true },
     password: { type: String, required: true },
     perfil: { type: String, default: perfil.png, required: true },
     telefono: { type: String, required: false },
     genero: { type: String, required: false },
-    f_nacimiento: { type: String, required: false },
+    fnacimiento: { type: String, required: false },
     dni: { type: String, required: false },
 });
 
-module.exports = mongoose.model("cliente", ClienteSchema);
+module.exports = mongoose.model("Cliente", ClienteSchema);
