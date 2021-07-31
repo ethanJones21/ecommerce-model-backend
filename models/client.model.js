a78789042b9d35a2620c47e092dac7bc646fe1d5
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-31 12:54:02 -0500

    corrigiendo a ingles

---
+++ models/client.model.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Schema, model } = require("mongoose");
+
+const ClientSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        lastname: { type: String, required: true },
+        country: { type: String, required: false },
+        email: { type: String, required: true },
+        pass: { type: String, required: true },
+        profile: { type: String, default: "assets/noimage.jpg" },
+        phone: { type: String, required: false },
+        gender: { type: String, required: false },
+        birthday: { type: String, required: false },
+        dni: { type: String, required: false },
+        active: { type: Boolean, default: true, required: true },
+        test: { type: Boolean, default: false, required: true },
+    },
+    { collection: "clients" }
+);
+
+ClientSchema.method("toJSON", function () {
+    const { __v, _id, pass, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+});
+
+module.exports = model("Client", ClientSchema);
